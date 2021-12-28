c686be0b70299e96cf85401c3f0378aa03a38f2f
Francis Polignano <franchyze923@gmail.com>
2021-12-27 17:36:37 -0800

    Update FranArchivesBackup.bat

--- FranArchivesBackup.bat
+++ FranArchivesBackup.bat
@@ -1,6 +1,6 @@
 ECHO Begin FranArchives Backup > "C:\backup\backuplog_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log" 2>&1
 robocopy \\xxx.xxx.x.xxx\FranArchives\ X:\ /MIR /FFT /R:0 /MT:4 /XA:SH /XD \\xxx.xxx.x.xxx\FranArchives\Media\ >> "C:\backup\backuplog_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log" 2>&1
-ECHO Finished backup of everything execept Media >> "C:\backup\backuplog_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log" 2>&1
+ECHO Finished backup of everything except Media >> "C:\backup\backuplog_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log" 2>&1
 robocopy \\xxx.xxx.x.xxx\FranArchives\Media\ Y:\ /MIR /FFT /R:0 /MT:4 /XA:SH >> "C:\backup\backuplog_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log" 2>&1
 ECHO Finished backup of Media to Y drive >> "C:\backup\backuplog_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log" 2>&1
 
