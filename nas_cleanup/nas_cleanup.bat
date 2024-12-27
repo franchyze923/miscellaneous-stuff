acfe3d6db45b6e7feea73cd6563b6ed5ced2707d
franchyze923 <franchyze923@gmail.com>
2024-12-27 13:21:39 -0500

    Create nas_cleanup.bat

---
+++ nas_cleanup/nas_cleanup.bat
@@ -0,0 +1,7 @@
+cd /d "C:\Users\franp\Desktop\nas_cleanup"
+
+call "C:\Users\franp\Desktop\nas_cleanup\nas_cleanup_venv\Scripts\activate.bat"
+
+python nas_cleanup.py
+
+echo Successfully ran NAS clean up script!
